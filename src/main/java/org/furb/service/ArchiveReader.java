9d89338857b69dfe6d12ffed2e11bd65d7c38684
Carlos Soler <carlinhoscaco09@gmail.com>
2024-10-30 20:20:47 -0300

    feat: add view screen

--- src/main/java/org/furb/service/ArchiveReader.java
+++ src/main/java/org/furb/service/ArchiveReader.java
@@ -49,4 +49,9 @@ public class ArchiveReader {
         }
         return stringsFromObjectList;
     }
+
+    public HtmlValidator getHtmlValidator() {
+        return htmlValidator;
+    }
+    
 }
